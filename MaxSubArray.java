21f0d573cf4124658585d8fa97a038b34a8d427b
shaurya <shauryatiwari120@gmail.com>
2025-03-20 23:34:03 +0530

    .

--- MaxSubArray.java
+++ MaxSubArray.java
@@ -23,29 +23,4 @@ public class MaxSubArray {
 
 }
 
-// public class MaxSubArray {
-// public int maxSubArray(int[] nums) {
-// int result = nums[0];
-// for (int i = 0; i < nums.length; i++) {
-// int sum = 0;
 
-// // Inner loop for ending point of subarray
-// for (int j = i; j < nums.length; j++) {
-// sum = sum + nums[j];
-
-// // update result if sum is greater the result
-// result = Math.max(result, sum);
-// }
-
-// }
-
-// return result;
-// }
-
-// public static void main(String[] args) {
-// MaxSubArray maxSubArray = new MaxSubArray();
-// int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-// System.out.println(maxSubArray.maxSubArray(nums));
-
-// }
-// }
