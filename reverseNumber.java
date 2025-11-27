6635c7876c6810398adcbdb60c86bbc3494202e7
shaurya <shauryatiwari120@gmail.com>
2025-11-28 01:09:35 +0530

    digit xtraction

--- reverseNumber.java
+++ reverseNumber.java
@@ -1,4 +1,6 @@
 // logic -> extraction of digit 
+// time complexity = log10(n) -> if any division is hapen with log1,lo2,logn , then it will became logn(n)
+import java.util.*;
 class revernumberr {
     public int reverseNumber(int n) {
         if (n == 0)
@@ -9,7 +11,8 @@ class revernumberr {
 
         while (n > 0) {// till n becames 0 
             int lastdigit = n % 10;
-            reverse = reverse * 10 + lastdigit;
+        
+            reverse = reverse * 10 + lastdigit; // this is concatination not addition 
             n/=10; // now the n is n.10 becaouse the last digit is removed now , like 234 ->23,4 , now -> 23 then ittration will start on this . 
         }
         return reverse;
